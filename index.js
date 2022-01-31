ec5b49e540e8d129bad2f07a00caa29f0587ce30
Edward Marion <edmarion6@gmail.com>
2022-01-31 13:34:49 -0500

    commit tree 37370c52b242477b7b30708dfd2dcdec74840e9d

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(dist){
   // Write your code here!
+  let freeRide, cost, sorry;
+
+  if (dist > 2500) {
+      sorry = 'No can do.';
+      return sorry;
+  } else if (dist > 2000) {
+    cost = 'I will gladly take your thirty bucks.';
+    return cost;
+  } else if (dist <= 400 ) {
+    freeRide = 'This one is on me!';
+    return freeRide;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(service){
   // Write your code here!
+  switch (service) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default: 
+      return 'Bye.';
+  }
 }
